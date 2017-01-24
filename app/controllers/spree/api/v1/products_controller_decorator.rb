9616486adca7044d2cf70f668f122abc43f539c3
Eaven P <eaven@popstand.com>
2017-01-23 16:09:11 -0800

    include selected_sizes in return of products for logged in users

--- app/controllers/spree/api/v1/products_controller_decorator.rb
+++ app/controllers/spree/api/v1/products_controller_decorator.rb
@@ -85,38 +85,41 @@ module Spree
           # then if no search query
           # we build a collection based on the users set prefernces
           # if user has no preferences set we grab all products
+          if params.has_key?(:in_taxons) or params.has_key?(:q)
+            if params.has_key?(:q)
+              product_ids = []
+              product_ids.concat(Spree::Product.all.in_name_or_description(params[:q]).pluck(:id))
 
-          if params.has_key?(:q)
-            product_ids = []
-            product_ids.concat(Spree::Product.all.in_name_or_description(params[:q]).pluck(:id))
-
-            if (brands_retailers = Spree::Taxon.where("name ILIKE ?", "%#{params[:q]}%")).present?
-              brands_retailers.each do |br|
-                product_ids.concat(br.products.pluck(:id))
+              if (brands_retailers = Spree::Taxon.where("name ILIKE ?", "%#{params[:q]}%")).present?
+                brands_retailers.each do |br|
+                  product_ids.concat(br.products.pluck(:id))
+                end
               end
+
+              @products = Spree::Product.where(id: product_ids.uniq)
             end
 
-            @products = Spree::Product.where(id: product_ids.uniq)
-          end
-
-          if params.has_key?(:in_taxons)
-            taxon_ids = params[:in_taxons].split(',').map(&:to_i)
-            @products = params.has_key?(:q) ? @products.in_taxons(taxon_ids) : Spree::Product.all.in_taxons(taxon_ids)
+            if params.has_key?(:in_taxons)
+              taxon_ids = params[:in_taxons].split(',').map(&:to_i)
+              @products = params.has_key?(:q) ? @products.in_taxons(taxon_ids) : Spree::Product.all.in_taxons(taxon_ids)
+            else
+              @products = Spree::Product.all unless params.has_key?(:q)
+            end
           else
-            @products = Spree::Product.all unless params.has_key?(:q)
-          end
-
-          #if (selected_sizes = current_api_user.preferences["selected_sizes"]).present?
-          #  product_ids = []
-          #  selected_sizes.keys.each do |taxon|
-          #    selected_sizes[taxon].keys.each do |option_type|
-          #      selected_sizes[taxon][option_type].each do |option_value|
-          #        product_ids.concat(Spree::Product.with_option_value(option_type, option_value).in_taxons(taxon.to_i).pluck(:id))
-          #      end
-          #    end
-          #  end
-          #  @products = Spree::Product.where(id: product_ids.uniq)
-          #end
+            if (selected_sizes = current_api_user.preferences["selected_sizes"]).present?
+              product_ids = []
+              selected_sizes.keys.each do |taxon|
+                selected_sizes[taxon].keys.each do |option_type|
+                  selected_sizes[taxon][option_type].each do |option_value|
+                    product_ids.concat(Spree::Product.with_option_value(option_type, option_value).in_taxons(taxon.to_i).pluck(:id))
+                  end
+                end
+              end
+              @products = Spree::Product.where(id: product_ids.uniq)
+            else
+              @products = Spree::Product.all
+            end
+          end              
 
           if @products.present?
             # Order products from newest to oldest
